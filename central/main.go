200403d68950366cf70e3fee38ae5c2805017bb6
UsrBern <bernardo.pinninghoff@usm.cl>
2023-09-08 23:37:27 -0300

    Mas progreso, hay un error con main.go en central

--- central/main.go
+++ central/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net"
 
+	"github.com/Sistemas-Distribuidos-2023-02/Grupo15-Laboratorio-1/tree/main/central/proto"
 	"google.golang.org/grpc"
 )
 
@@ -13,7 +14,11 @@ func main() {
 		log.Fatal("Failed to listen on port 9000: %v", err)
 	}
 
+	s := central.Server{}
+
 	grpcServer := grpc.NewServer()
+
+	central.RegisterCentralServer(grpcServer, &s)
 
 	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatal("Failed to serve gRPC server over port 9000: %v", err)
