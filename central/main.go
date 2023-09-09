900ea8256bf6b5f80d994a04e4bf41ad821ee23a
UsrBern <bernardo.pinninghoff@usm.cl>
2023-09-09 18:30:11 -0300

    keygen listo

--- central/main.go
+++ central/main.go
@@ -8,6 +8,8 @@ import (
 	"io/ioutil"
 	"strings"
 	"strconv"
+	"math/rand"
+	"time"
 
 	"google.golang.org/grpc"
 )
@@ -38,6 +40,27 @@ func startupParameters(filePath string) (minKey, maxKey int, err error) {
 	return minKey, maxKey, nil
 }
 
-func main() {
+func keygen(minKey, maxKey int) []int {
+	rand.Seed(time.Now().UnixNano())
 
+	n := rand.Intn(maxKey - minKey + 1) + minKey
+
+	keys := make([]int, n)
+	for i := 0; i < n; i++ {
+		keys[i] = minKey + rand.Intn(n)
+	}
+	return keys
+}
+
+func main() {
+	filePath := "./parametros_de_inicio.txt"
+
+	minKey, maxKey, err := startupParameters(filePath)
+	if err != nil {
+		fmt.Printf("Error reading startup_parameters: %v\n", err)
+		return
+	}
+
+	keys := keygen(minKey, maxKey)
+	fmt.Printf("Keys: %v\n", keys)
 }
