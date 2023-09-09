4865553a67115f832b7237216266991f291261fa
UsrBern <bernardo.pinninghoff@usm.cl>
2023-09-09 14:21:21 -0300

    Cambios a central.proto

--- central/main.go
+++ central/main.go
@@ -1,5 +1,16 @@
 package main
 
 import (
-	pb "github.com/Sistemas-Distribuidos-2023-02/Grupo15-Laboratorio-1/central/proto"
+	"log"
+	"context"
+	pb "github.com/Sistemas-Distribuidos-2023-02/Grupo15-Laboratorio-1/tree/main/central/proto"
 )
+
+type server struct {
+	pb.UnimplementedCentralServer
+}
+
+func (s *server) Keygen(ctx context.Context, message *pb.Message) (*pb.Message, error) {
+	log.Printf("Received message body from client: %s", message.Body)
+	return &pb.Message{Body: "0"}, nil
+}
