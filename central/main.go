1c2eb9dcc6ecaf0d1867924e3678f092a1f96f4b
UsrBern <bernardo.pinninghoff@usm.cl>
2023-09-09 14:44:49 -0300

    CORRE EL SERVIDOR BIEN CTM

--- central/main.go
+++ central/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
-	"log"
 	"context"
+	"log"
+	"net"
+
 	pb "github.com/Sistemas-Distribuidos-2023-02/Grupo15-Laboratorio-1/tree/main/central/proto"
+	"google.golang.org/grpc"
 )
 
 type server struct {
@@ -13,4 +16,20 @@ type server struct {
 func (s *server) Keygen(ctx context.Context, message *pb.Message) (*pb.Message, error) {
 	log.Printf("Received message body from client: %s", message.Body)
 	return &pb.Message{Body: "0"}, nil
+}
+
+func main() {
+	listener, err := net.Listen("tcp", ":8080")
+
+	if err != nil {
+		panic("cannot create tcp server" + err.Error())
+	}
+
+	serv := grpc.NewServer()
+
+	pb.RegisterCentralServer(serv, &server{})
+
+	if err = serv.Serve(listener); err != nil {
+		panic("cannot initialize grpc server" + err.Error())
+	}
 }
