f33953ce5cc6fe3cd1dd01e01c1cbc5201958810
UsrBern <bernardo.pinninghoff@usm.cl>
2023-09-09 14:12:07 -0300

    Empezando main.go de 0

--- central/main.go
+++ central/main.go
@@ -1,26 +1,5 @@
 package main
 
 import (
-	"log"
-	"net"
-
-	"github.com/Sistemas-Distribuidos-2023-02/Grupo15-Laboratorio-1/tree/main/central/proto"
-	"google.golang.org/grpc"
+	pb "github.com/Sistemas-Distribuidos-2023-02/Grupo15-Laboratorio-1/central/proto"
 )
-
-func main() {
-	lis, err := net.Listen("tcp", ":9000")
-	if err != nil {
-		log.Fatal("Failed to listen on port 9000: %v", err)
-	}
-
-	s := central.Server{}
-
-	grpcServer := grpc.NewServer()
-
-	central.RegisterCentralServer(grpcServer, &s)
-
-	if err := grpcServer.Serve(lis); err != nil {
-		log.Fatal("Failed to serve gRPC server over port 9000: %v", err)
-	}
-}
