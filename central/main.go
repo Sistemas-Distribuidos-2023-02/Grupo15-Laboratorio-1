b3d8e7fcbb69f9251ee1b56ab1ef35e6456dced0
UsrBern <bernardo.pinninghoff@usm.cl>
2023-09-10 18:37:09 -0300

    RabbitMQ fix

--- central/main.go
+++ central/main.go
@@ -9,14 +9,9 @@ import (
 	"strconv"
 	"strings"
 	"time"
-	"os"
-	"os/signal"
-	"syscall"
-	"sync"
 
 	"github.com/Sistemas-Distribuidos-2023-02/Grupo15-Laboratorio-1/proto/betakeys"
 	"github.com/streadway/amqp"
-	"google.golang.org/protobuf/proto"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 	"google.golang.org/protobuf/types/known/emptypb"
@@ -64,6 +59,38 @@ func keygen(minKey, maxKey int) []int {
 		keys[i] = minKey + rand.Intn(n)
 	}
 	return keys
+}
+
+func setupRabbitMQ()(*amqp.Channel, error){
+	//RabbitMQ server connection
+	const rabbitmqURL = "amqp://guest:guest@localhost:25672/"
+
+	rabbitConn, err := amqp.Dial(rabbitmqURL)
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to rabbitmq server: %v", err)
+	}
+
+	rabbitChannel, err := rabbitConn.Channel()
+	if err != nil {
+		return nil, fmt.Errorf("failed to open a rabbitmq channel: %v", err)
+	}
+
+	// RabbitMQ queue declaration
+	queueName := "keyVolunteers"
+
+	_, err = rabbitChannel.QueueDeclare(
+		queueName,
+		false,	// durable
+		false,	// delete when unused
+		false,	// exclusive
+		false,	// no-wait
+		nil,	// arguments
+	)
+	if err != nil {
+		return nil, fmt.Errorf("failed to declare rabbitmq queue: %v", err)
+	}
+
+	return rabbitChannel, nil
 }
 
 func main() {
@@ -118,94 +145,12 @@ func main() {
 		return
 	}
 
-	//RabbitMQ server connection
-	const rabbitmqURL = "amqp://guest:guest@localhost:25672/"
-
-	rabbitConn, err := amqp.Dial(rabbitmqURL)
+	// Set up RabbitMQ
+	rabbitChannel, err := setupRabbitMQ()
 	if err != nil {
-		fmt.Printf("Failed to connect to RabbitMQ server: %v\n", err)
-		return
-	}
-	defer rabbitConn.Close()
-
-	rabbitChannel, err := rabbitConn.Channel()
-	if err != nil {
-		fmt.Printf("Failed to open a RabbitMQ channel: %v\n", err)
+		fmt.Printf("Failed to set up RabbitMQ: %v\n", err)
 		return
 	}
 	defer rabbitChannel.Close()
-
-	// RabbitMQ queue declaration
-	queueName := "keyVolunteers"
-
-	_, err = rabbitChannel.QueueDeclare(
-		queueName,
-		false,	// durable
-		false,	// delete when unused
-		false,	// exclusive
-		false,	// no-wait
-		nil,	// arguments
-	)
-	if err != nil {
-		fmt.Printf("Failed to declare RabbitMQ queue: %v\n", err)
-		return
-	}
-
-	RabbitShutdownChannel := make(chan os.Signal, 1)
-	signal.Notify(RabbitShutdownChannel, syscall.SIGINT, syscall.SIGTERM)
-	var wg sync.WaitGroup
-
-	// Asynchronous message reception
-	wg.Add(1)
-	go func ()  {
-		// Consuming message
-		msgs, err := rabbitChannel.Consume(
-			queueName,
-			"",		// consumer
-			true,	// auto-ack
-			false,	// exclusive
-			false,	// no-local
-			false,	// no-wait
-			nil,	// arguments
-		)
-		if err != nil {
-			fmt.Printf("Failed to register a regional server user: %v\n", err)
-			return
-		}
-
-		for {
-			select{
-				case <- RabbitShutdownChannel:
-					fmt.Printf("RabbitMQ server shutting down gracefully...\n")
-					wg.Done()
-					return
-				case msg := <- msgs:
-					// Unmarshalling message
-					var reply betakeys.RegionalKeyReply
-
-					err := proto.Unmarshal(msg.Body, &reply)
-					if err != nil {
-						fmt.Printf("Failed to unmarshal message: %v\n", err)
-						return
-					}
-			
-					serverName := reply.ServerName
-					content := reply.Content
-			
-					fmt.Printf("Mensaje asincrono de servidor %v leido: %v\n", serverName, content)
-					msg.Ack(false)
-					wg.Done()
-			}
-
-		}
-		
-	} ()
-	
-	// Waiting for shutdown signal
-	<- RabbitShutdownChannel
-
-	// Graceful shutdown
-	wg.Wait()
-	rabbitChannel.Close()
 	
 }
